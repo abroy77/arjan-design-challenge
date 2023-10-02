36d8cb776e06386526e6ada2778cf426961814db
Abhishek Roy <abhishek.roy@perspectum.com>
2023-10-03 00:15:54 +0100

    correcting misnaming of files

--- challenge_18_refactoring/after/main.py
+++ challenge_18_refactoring/after/main.py
@@ -7,9 +7,36 @@ from weather import (
     get_wind_speed,
     get_wind_direction,
 )
+from enum import Enum
 
 
-def main() -> None:
+class Condition(Enum):
+    ALL = "all"
+    TEMPERATURE = "temperature"
+    HUMIDITY = "humidity"
+    WIND = "wind"
+
+
+OPTION_TO_CONDITION = {
+    "all": Condition.ALL,
+    "a": Condition.ALL,
+    "temperature": Condition.TEMPERATURE,
+    "t": Condition.TEMPERATURE,
+    "humidity": Condition.HUMIDITY,
+    "h": Condition.HUMIDITY,
+    "wind": Condition.WIND,
+    "w": Condition.WIND,
+}
+
+CONDITION_TO_FUNCTION = {
+    Condition.TEMPERATURE: get_temperature,
+    Condition.HUMIDITY: get_humidity,
+    Condition.WIND: get_wind_speed,
+}
+
+
+def parse_args() -> argparse.Namespace:
+
     parser = argparse.ArgumentParser(
         description="Get the current weather information for a city"
     )
@@ -23,9 +50,8 @@ def main() -> None:
         metavar="CONDITION",
         nargs="+",
         default=["temperature"],
-        choices=["all", "a", "temperature", "t", "humidity", "h", "wind", "w"],
-        help="Weather conditions to display. Choose between 'all' or 'a', 'temperature' or 't', "
-        "'humidity' or 'h', 'wind' or 'w'.",
+        choices=OPTION_TO_CONDITION.keys(),
+        help=f"Weather conditions to display. Choose between {OPTION_TO_CONDITION.keys()}.",
     )
 
     parser.add_argument(
@@ -38,46 +64,43 @@ def main() -> None:
 
     if not args.api_key:
         # That will not happen because of the API default value.
-        print("Please provide an API key with the --api-key option.")
-        return
+        raise ValueError("Please provide an API key with the --api-key option.")
 
-    if args.conditions:
-        # Fetch the data from the OpenMapWeather API
-        weather_forecast = get_complete_forecast(
-            http_get_fn=http_get, api_key=args.api_key, city=args.city
+    return args
+
+
+def main() -> None:
+    args = parse_args()
+
+    if not args.conditions:
+        #  This will never happen because temperature is set as the default condition.
+        raise ValueError(
+            "Please specify at least one weather condition to display with the --conditions option."
+        )
+    # Fetch the data from the OpenMapWeather API
+    weather_forecast = get_complete_forecast(
+        http_get_fn=http_get, api_key=args.api_key, city=args.city
+    )
+    if not weather_forecast:
+        raise ValueError(
+            f"Couldn't find weather data. Check '{args.city}' if it exists and is correctly spelled.\n"
         )
 
-        for condition in args.conditions:
-            if condition in ["temperature", "t"]:
-                temperature = get_temperature(weather_forecast)
-                print(
-                    f"The current temperature in {args.city} is {temperature:.1f} °C."
-                )
-            elif condition in ["humidity", "h"]:
-                print(
-                    f"The current humidity in {args.city} is {get_humidity(weather_forecast)}%."
-                )
-            elif condition in ["wind", "w"]:
-                print(
-                    f"The current wind speed in {args.city} is {get_wind_speed(weather_forecast)} m/s "
-                    f"from direction {get_wind_direction(weather_forecast)} degrees."
-                )
-            else:
-                temperature = get_temperature(weather_forecast)
-                print(
-                    f"The current temperature in {args.city} is {temperature:.1f} °C."
-                )
-                print(
-                    f"The current humidity in {args.city} is {get_humidity(weather_forecast)}%."
-                )
-                print(
-                    f"The current wind speed in {args.city} is {get_wind_speed(weather_forecast)} m/s "
-                    f"from direction {get_wind_direction(weather_forecast)} degrees."
-                )
-    else:
-        # This will never happen because temperature is set as the default condition.
+    conditions = set([OPTION_TO_CONDITION[option] for option in args.conditions])
+
+    if Condition.ALL in conditions or Condition.TEMPERATURE in conditions:
+        temperature = get_temperature(weather_forecast)
         print(
-            f"Please specify at least one weather condition to display with the --conditions option."
+            f"The current temperature in {args.city} is {temperature:.1f} °C."
+        )
+    if Condition.ALL in conditions or Condition.HUMIDITY in conditions:
+        print(
+            f"The current humidity in {args.city} is {get_humidity(weather_forecast)}%."
+        )
+    if Condition.ALL in conditions or Condition.WIND in conditions:
+        print(
+            f"The current wind speed in {args.city} is {get_wind_speed(weather_forecast)} m/s "
+            f"from direction {get_wind_direction(weather_forecast)} degrees."
         )
 
 
--- challenge_18_refactoring/before/main.py
+++ challenge_18_refactoring/before/main.py
@@ -7,36 +7,9 @@ from weather import (
     get_wind_speed,
     get_wind_direction,
 )
-from enum import Enum
 
 
-class Condition(Enum):
-    ALL = "all"
-    TEMPERATURE = "temperature"
-    HUMIDITY = "humidity"
-    WIND = "wind"
-
-
-OPTION_TO_CONDITION = {
-    "all": Condition.ALL,
-    "a": Condition.ALL,
-    "temperature": Condition.TEMPERATURE,
-    "t": Condition.TEMPERATURE,
-    "humidity": Condition.HUMIDITY,
-    "h": Condition.HUMIDITY,
-    "wind": Condition.WIND,
-    "w": Condition.WIND,
-}
-
-CONDITION_TO_FUNCTION = {
-    Condition.TEMPERATURE: get_temperature,
-    Condition.HUMIDITY: get_humidity,
-    Condition.WIND: get_wind_speed,
-}
-
-
-def parse_args() -> argparse.Namespace:
-
+def main() -> None:
     parser = argparse.ArgumentParser(
         description="Get the current weather information for a city"
     )
@@ -50,8 +23,9 @@ def parse_args() -> argparse.Namespace:
         metavar="CONDITION",
         nargs="+",
         default=["temperature"],
-        choices=OPTION_TO_CONDITION.keys(),
-        help=f"Weather conditions to display. Choose between {OPTION_TO_CONDITION.keys()}.",
+        choices=["all", "a", "temperature", "t", "humidity", "h", "wind", "w"],
+        help="Weather conditions to display. Choose between 'all' or 'a', 'temperature' or 't', "
+        "'humidity' or 'h', 'wind' or 'w'.",
     )
 
     parser.add_argument(
@@ -64,39 +38,49 @@ def parse_args() -> argparse.Namespace:
 
     if not args.api_key:
         # That will not happen because of the API default value.
-        raise ValueError("Please provide an API key with the --api-key option.")
+        print("Please provide an API key with the --api-key option.")
+        return
 
-    return args
+    if args.conditions:
+        # Fetch the data from the OpenMapWeather API
+        weather_forecast = get_complete_forecast(
+            http_get_fn=http_get, api_key=args.api_key, city=args.city
+        )
 
+        if not weather_forecast:
+            raise ValueError("Forecast for the city not found.")
 
-def main() -> None:
-    args = parse_args()
-
-    if not args.conditions:
-        #  This will never happen because temperature is set as the default condition.
-        raise ValueError(
+        for condition in args.conditions:
+            if condition in ["temperature", "t"]:
+                temperature = get_temperature(weather_forecast)
+                print(
+                    f"The current temperature in {args.city} is {temperature:.1f} °C."
+                )
+            elif condition in ["humidity", "h"]:
+                print(
+                    f"The current humidity in {args.city} is {get_humidity(weather_forecast)}%."
+                )
+            elif condition in ["wind", "w"]:
+                print(
+                    f"The current wind speed in {args.city} is {get_wind_speed(weather_forecast)} m/s "
+                    f"from direction {get_wind_direction(weather_forecast)} degrees."
+                )
+            else:
+                temperature = get_temperature(weather_forecast)
+                print(
+                    f"The current temperature in {args.city} is {temperature:.1f} °C."
+                )
+                print(
+                    f"The current humidity in {args.city} is {get_humidity(weather_forecast)}%."
+                )
+                print(
+                    f"The current wind speed in {args.city} is {get_wind_speed(weather_forecast)} m/s "
+                    f"from direction {get_wind_direction(weather_forecast)} degrees."
+                )
+    else:
+        # This will never happen because temperature is set as the default condition.
+        print(
             "Please specify at least one weather condition to display with the --conditions option."
-        )
-    # Fetch the data from the OpenMapWeather API
-    weather_forecast = get_complete_forecast(
-        http_get_fn=http_get, api_key=args.api_key, city=args.city
-    )
-
-    conditions = set([OPTION_TO_CONDITION[option] for option in args.conditions])
-
-    if Condition.ALL in conditions or Condition.TEMPERATURE in conditions:
-        temperature = get_temperature(weather_forecast)
-        print(
-            f"The current temperature in {args.city} is {temperature:.1f} °C."
-        )
-    if Condition.ALL in conditions or Condition.HUMIDITY in conditions:
-        print(
-            f"The current humidity in {args.city} is {get_humidity(weather_forecast)}%."
-        )
-    if Condition.ALL in conditions or Condition.WIND in conditions:
-        print(
-            f"The current wind speed in {args.city} is {get_wind_speed(weather_forecast)} m/s "
-            f"from direction {get_wind_direction(weather_forecast)} degrees."
         )
 
 
